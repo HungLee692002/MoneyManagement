dd85029b28b039a695a613cab80c0b335fc15a18
HungLee692002 <hungle692002@gmail.com>
2024-08-21 00:29:00 +0700

    first commit

---
+++ MoneyManagement/UserControls/MenuButton.xaml.cs
@@ -0,0 +1,38 @@
+﻿using FontAwesome.WPF;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace MoneyManagement.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MenuButton.xaml
+    /// </summary>
+    public partial class MenuButton : UserControl
+    {
+        public MenuButton()
+        {
+            InitializeComponent();
+        }
+
+        public string Caption
+        {
+            get { return (string)GetValue(CaptionProperty); }
+            set { SetValue(CaptionProperty, value); }
+        }
+
+        public static readonly DependencyProperty CaptionProperty = DependencyProperty.Register("Caption", typeof(string), typeof(MenuButton));
+
+        public FontAwesomeIcon Icon
+        {
+            get { return (FontAwesomeIcon)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(FontAwesomeIcon), typeof(MenuButton));
+    }
+
+    
+
+}
